4d11f4dbbe61a7ab85998ebad92e7c7a604c7640
Saurav <saurav.dutta@naiacorp.net>
2023-01-17 21:50:45 +0530

    GMCB-138: Form design refactoring

--- src/app/billing/work-queue-poc-form/work-queue-poc-form.component.ts
+++ src/app/billing/work-queue-poc-form/work-queue-poc-form.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
-import { FORM_DATA, formData } from './constants';
+import {FORM_DATA, formData} from './constants';
 
 @Component({
   selector: 'app-work-queue-poc-form',
@@ -9,4 +9,49 @@ import { FORM_DATA, formData } from './constants';
 })
 export class WorkQueuePocFormComponent {
   formData: formData[] = FORM_DATA;
+
+  selectedRoles: any = [];
+  selectedCollections: any = [];
+  selectedFields: any = [];
+  selectedActions: any = [];
+
+  onRoleClicked(type: string, role: string) {
+    if (type === 'available') {
+      if (!this.selectedRoles.includes(role)) {
+        this.selectedRoles.push(role);
+      }
+    } else {
+      this.selectedRoles.splice(this.selectedRoles.indexOf(role), 1);
+    }
+  }
+
+  onCollectionClicked(type: string, role: string){
+    if (type === 'available') {
+      if (!this.selectedCollections.includes(role)) {
+        this.selectedCollections.push(role);
+      }
+    } else {
+      this.selectedCollections.splice(this.selectedCollections.indexOf(role), 1);
+    }
+  }
+
+  onFieldClicked(type: string, role: string){
+    if (type === 'available') {
+      if (!this.selectedFields.includes(role)) {
+        this.selectedFields.push(role);
+      }
+    } else {
+      this.selectedFields.splice(this.selectedFields.indexOf(role), 1);
+    }
+  }
+
+  onActionClicked(type: string, role: string){
+    if (type === 'available') {
+      if (!this.selectedActions.includes(role)) {
+        this.selectedActions.push(role);
+      }
+    } else {
+      this.selectedActions.splice(this.selectedActions.indexOf(role), 1);
+    }
+  }
 }
